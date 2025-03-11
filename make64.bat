b3018993de38e9cc77ff99da8429877e6b669e5e
ivansoft <32499694+ivansoft@users.noreply.github.com>
2025-03-11 04:20:20 +0300

    make64.bat options /build /install /env

--- make64.bat
+++ make64.bat
@@ -1,5 +1,6 @@
+:: https://github.com/openssl/openssl/blob/master/NOTES-WINDOWS.md
 @echo off
-setlocal
+if /I NOT "%1" == "/ENV" setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
 set PATH=%~dp0\externals\nasm;%PATH%
@@ -8,16 +9,49 @@ call "%~dp0\externals\perl\portableshell.bat" /SETENV
 perl -MConfig -e "print(qq{Perl executable: $^X\nPerl version   : $^V / $Config{archname}\n\n})" 2>nul
 if ERRORLEVEL==1 (
 	echo FATAL ERROR: 'perl' does not work; check if your strawberry pack is complete!
-	goto END
+	goto ENDLOCAL
 )
+if /I "%1" == "/ENV" goto EOF
 
 set INSTALL_DIR=%~dp0
-:: https://github.com/openssl/openssl/blob/master/NOTES-WINDOWS.md
+
 pushd "%~dp0\src"
-	perl Configure VC-WIN64A
-	nmake
-	nmake install --prefix %INSTALL_DIR%
-popd
+
+set "_CONFIGBUILD="
+if /I "%1" == "/BUILD" goto BUILD
+if /I "%1" == "/INSTALL" goto INSTALL
+if /I "%1" == "" set _CONFIGBUILD=yes
+
+:CONFIG
+echo ----------------------------------------------------------
+perl Configure --prefix=%INSTALL_DIR% --openssldir=%INSTALL_DIR%ssl VC-WIN64A
+
+if ERRORLEVEL==1 goto END
+if NOT "%_CONFIGBUILD%" == "" goto BUILD
+goto END
+
+:BUILD
+echo ----------------------------------------------------------
+echo %CD%
+:: NMAKE : fatal error U1065: invalid option '-'
+:: The error "NMAKE : fatal error U1065: invalid option '-'" typically occurs when building applications using Microsoft Visual C++ tools, and it is often related to an improperly set MAKEFLAGS environment variable
+set MAKEFLAGS=
+nmake
+
+if ERRORLEVEL==1 goto END
+if NOT "%_CONFIGBUILD%" == "" goto INSTALL
+goto END
+
+:INSTALL
+echo ----------------------------------------------------------
+echo %CD%
+set MAKEFLAGS=
+nmake install
 
 :END
+popd
+
+:ENDLOCAL
 endlocal
+
+:EOF
